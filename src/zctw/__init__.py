82ca78a2a34446d505a0d6ac73a3eaddefcf2c90
Darío Clavijo <clavijodario@gmail.com>
2026-04-09 13:12:49 -0300

    Bump version to 0.1.1

--- src/zctw/__init__.py
+++ src/zctw/__init__.py
@@ -1,6 +1,6 @@
 """zctw - Python implementation of Context Tree Weighting (CTW) compression."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["CTWCompressor", "CTWSettings"]
 
 from zctw._compressor import CTWCompressor, CTWSettings
